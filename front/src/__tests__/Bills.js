823a5d8af9d97f69b5c8e4077653a25aec64d85f
FabienStervinou <stervinoufabien@gmail.com>
2022-03-22 08:28:23 +0100

    TU <bills> add missing expect for test -- Then bill icon in vertical layout should be highlighted

--- front/src/__tests__/Bills.js
+++ front/src/__tests__/Bills.js
@@ -12,8 +12,8 @@ import router from "../app/Router.js";
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
-    test("Then bill icon in vertical layout should be highlighted", async () => {
 
+    test("Then bill icon in vertical layout should be highlighted", async () => {
       Object.defineProperty(window, 'localStorage', { value: localStorageMock })
       window.localStorage.setItem('user', JSON.stringify({
         type: 'Employee'
@@ -24,10 +24,9 @@ describe("Given I am connected as an employee", () => {
       router()
       window.onNavigate(ROUTES_PATH.Bills)
       await waitFor(() => screen.getByTestId('icon-window'))
-      const windowIcon = screen.getByTestId('icon-window')
-      //to-do write expect expression
-
+      expect(screen.getByTestId('icon-window')).toEqual(document.querySelector('.active-icon'))
     })
+
     test("Then bills should be ordered from earliest to latest", () => {
       document.body.innerHTML = BillsUI({ data: bills })
       const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
